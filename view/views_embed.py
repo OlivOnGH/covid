b1478c106d333b3410497ae1e7be8dbbd26e2b0c
OlivOnGH <76781017+OlivOnGH@users.noreply.github.com>
2022-01-04 15:38:29 +0100

    add slots

--- view/views_embed.py
+++ view/views_embed.py
@@ -1,4 +1,4 @@
-"""Ce module d'enveloppe (wrapper) facilite l'utilisation de la bibliothèque discord.py 
+"""Ce module d'enveloppe (wrapper) facilite l'utilisation de la bibliothèque discord.py
 et la migration ultérieure vers une nouvelle bibliothèque.
 """
 
@@ -10,7 +10,7 @@ from pathlib import Path
 
 import discord
 
-from functions._local_datetime import local_dt
+from functions._local_datetime import local_dt_sync
 from functions._image_upload import image_upload
 from functions.a_threads import Threads
 
@@ -43,8 +43,10 @@ class EmbedView:
     color_hex:   int = None
     url:         str = None
     fields:      list = None
-    image_path:  str = None
+    image_path:  Path = None
     file:        tuple = None
+
+    __slots__ = ('__dict__', 'msg', 'embed_sans_image', 'embed', 'upload_link', 'image_url', 'file_discord', 'n', 'v', 'i')
 
     # def __post_init__(self):
     #     '''Convertir la couleur str() en int()'''
@@ -61,25 +63,22 @@ class EmbedView:
             Exception générale.
         """
         try:
-            embed = discord.Embed(title=f'**{self.title}**',
+            self.embed = discord.Embed(title=f'**{self.title}**',
                                   description=f'*{self.description}*',
                                   url=self.url,
                                   color=self.color_hex)
             if self.fields:  # [(Name, Value, True/False)]
-                [embed.add_field(name=f'**{n}**', value=f'{v}\n\u200b', inline=i) for n, v, i in self.fields]
-            embed.set_footer(text=self.footer)
-            embed.timestamp = await local_dt()
-            return embed
+                [self.embed.add_field(name=f'**{self.n}**', value=f'{self.v}\n\u200b', inline=self.i) for self.n, self.v, self.i in self.fields]
+            self.embed.set_footer(text=self.footer)
+            self.embed.timestamp = local_dt_sync()
+            return self.embed
         except Exception as err:
             exc_type, exc_obj, exc_tb = sys.exc_info()
             fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
             raise Exception(err, exc_type, fname, exc_tb.tb_lineno)
 
-    async def upload(self, image_path) -> str:
+    async def upload(self) -> str:
         """Téléverser l'image sur un serveur distant et obtenir son url l'URL.
-
-        Args:
-            image_path (Path): Chemin de l'image.
 
         Returns:
             upload_link (str): URL de l'image.
@@ -87,45 +86,49 @@ class EmbedView:
         Raises:
             Exception générale.
         """
-        upload_link = '-'  # Todo
         try:
-            if isinstance(image_path, str): image_path = Path(image_path)
-            upload_link = await image_upload(image_path)
+            if self.image_path:
+                return await image_upload(Path(self.image_path))
+            print('image_path', self.image_path)  # Todo : Logger
         except Exception as err:
             exc_type, exc_obj, exc_tb = sys.exc_info()
             fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
             raise Exception(err, exc_type, fname, exc_tb.tb_lineno)
+
+    async def embed_prep(self) -> None:
+        self.embed_sans_image = await self.create_embed()  # Créer l'embed sans image
+
+        if self.image_path:
+            self.embed = deepcopy(self.embed_sans_image)  # Puis l'embed avec image
+            self.image_url = Threads(self.upload())
+            image_url = self.image_url()
+            print('image_url', image_url)
+            self.embed.set_image(url=image_url)
         else:
-            print(upload_link)  # Todo : Logger
-        return upload_link
+            self.embed = self.embed_sans_image
 
     async def edit(self) -> None:
-        """Edition du message.
+        """Edition du message souhaité.
 
         Returns:
             None
 
-        Result:
-            Édite le message souhaité.
-
         Raises:
             Exception générale.
         """
-        msg = await self.bot.get_channel(self.salon_id).fetch_message(self.message_id)
-        async with msg.channel.typing():
-            embed_sans_image = await self.create_embed()  # Créer l'embed sans image
-            embed = deepcopy(embed_sans_image)                 # Puis l'embed avec image
-
-            if self.image_path:
-                image_url = Threads(self.upload(self.image_path))
-                embed.set_image(url=image_url())
+        self.msg = await self.bot.get_channel(self.salon_id).fetch_message(self.message_id)
+        async with self.msg.channel.typing():
+            await self.embed_prep()
 
             try:
-                await msg.edit(content=None, embed=embed)
+                await self.msg.edit(content=None, embed=self.embed)
+            except discord.errors.HTTPException as err:
+                print(err)
+                await self.msg.edit(content=None, embed=self.embed_sans_image)
             except Exception as err:
                 exc_type, exc_obj, exc_tb = sys.exc_info()
                 fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-                await msg.edit(content=None, embed=embed_sans_image)
+                await self.msg.edit(content=None, embed=self.embed_sans_image)
                 raise Exception(err, exc_type, fname, exc_tb.tb_lineno)
 
     async def send(self) -> None:
@@ -141,25 +144,23 @@ class EmbedView:
             Exception générale.
         """
         try:
-            channel = self.ctx.channel
+            self.msg = self.ctx.channel
         except AttributeError:
-            channel = self.bot.get_channel(self.salon_id)
+            self.msg = self.bot.get_channel(self.salon_id)
 
-        async with channel.typing():
-            embed_sans_image = await self.create_embed()  # Créer l'embed sans image
-            embed = deepcopy(embed_sans_image)  # Puis l'embed avec image
-
-            if self.image_path:
-                image_url = Threads(self.upload(self.image_path))
-                embed.set_image(url=image_url())
+        async with self.msg.typing():
+            await self.embed_prep()
 
             if self.file:
-                file_discord = discord.File(self.file[0], filename=self.file[1])
+                self.file_discord = discord.File(self.file[0], filename=self.file[1])
 
             try:
-                await channel.send(content=None, embed=embed, file=file_discord)
+                await self.msg.send(content=None, embed=self.embed, file=self.file_discord)
+            except discord.errors.HTTPException as err:
+                print(err)
+                await self.msg.edit(content=None, embed=self.embed_sans_image)
             except Exception as err:
                 exc_type, exc_obj, exc_tb = sys.exc_info()
                 fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-                await channel.send(content=None, embed=embed_sans_image)
+                await self.msg.send(content=None, embed=self.embed_sans_image)
                 raise Exception(err, exc_type, fname, exc_tb.tb_lineno)
