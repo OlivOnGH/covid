126c7adde7135715a3c57ddce27fd25e1cc37384
OlivOnGH <76781017+OlivOnGH@users.noreply.github.com>
2022-01-05 23:37:40 +0100

    image thumbnail

--- view/views_embed.py
+++ view/views_embed.py
@@ -29,24 +29,63 @@ async def embed_send_gc(*args, **kwargs) -> None:
     del embed
     gc.collect()
 
+async def upload_image_bdd(image_path, image_bdd_table, image_keep=False) -> str:
+    """Téléverser l'image sur un serveur distant et obtenir son url l'URL.
+
+    Returns:
+        upload_link (str): URL de l'image.
+
+    Raises:
+        Exception générale.
+    """
+    try:
+        # Si le chemin est un :str:, convertir en :Path:
+        if not isinstance(image_path, Path):
+            image_path = Path(image_path)
+
+        # Si on souhaite conserver l'image, vérifier si l'URL est déjà présente dans la BDD.
+        if image_keep:
+            image_nom, image_ext = os.path.basename(image_path).rsplit('.', 1)
+            image_dirpath = os.path.dirname(image_path)
+            print('Vérification dans la BDD de l\'image : ', image_nom)
+            from functions.f_mysql import Image
+            image = Image(table=image_bdd_table,
+                          name=image_nom,
+                          endpoint=image_dirpath,
+                          file_format=image_ext)
+            return await image()  # URL
+
+        # Sinon, téléverser l'image et retourner l'URL.
+        return await image_upload(image_path)  # URL  # Todo : Logger
+
+    except Exception as err:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        raise Exception(err, exc_type, fname, exc_tb.tb_lineno)
+
 
 @dataclass
 class EmbedView:
     '''Créer un embed.'''
-    bot:         discord
-    salon_id:    int = None
-    ctx:         discord.TextChannel = None
-    title:       str = '\u200b'
-    message_id:  int = None
-    description: str = '\u200b'
-    footer:      str = None
-    color_hex:   int = None
-    url:         str = None
-    fields:      list = None
-    image_path:  Path = None
-    file:        tuple = None
+    bot:                  discord
+    salon_id:             int = None
+    ctx:                  discord.TextChannel = None
+    title:                str = '\u200b'
+    message_id:           int = None
+    description:          str = '\u200b'
+    footer:               str = None
+    color_hex:            int = None
+    url:                  str = None
+    fields:               list = None
+    image_path:           Path = None
+    image_keep:           bool = False
+    image_bdd_table:      str = 'updates'
+    thumbnail_path:       Path = None
+    thumbnail_keep:       bool = False
+    thumbnail_bdd_table:  str = 'updates'
+    file:                 tuple = None
 
-    __slots__ = ('__dict__', 'msg', 'embed_sans_image', 'embed', 'upload_link', 'image_url', 'file_discord', 'n', 'v', 'i')
+    __slots__ = '__dict__', 
 
     # def __post_init__(self):
     #     '''Convertir la couleur str() en int()'''
@@ -77,33 +116,28 @@ class EmbedView:
             fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
             raise Exception(err, exc_type, fname, exc_tb.tb_lineno)
 
-    async def upload(self) -> str:
-        """Téléverser l'image sur un serveur distant et obtenir son url l'URL.
-
-        Returns:
-            upload_link (str): URL de l'image.
-
-        Raises:
-            Exception générale.
-        """
-        try:
-            if self.image_path:
-                return await image_upload(Path(self.image_path))
-            print('image_path', self.image_path)  # Todo : Logger
-        except Exception as err:
-            exc_type, exc_obj, exc_tb = sys.exc_info()
-            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-            raise Exception(err, exc_type, fname, exc_tb.tb_lineno)
-
     async def embed_prep(self) -> None:
+        """Prépare l'embed, que ce soit pour self.edit ou self.send."""
         self.embed_sans_image = await self.create_embed()  # Créer l'embed sans image
 
-        if self.image_path:
-            self.embed = deepcopy(self.embed_sans_image)  # Puis l'embed avec image
-            self.image_url = Threads(self.upload())
-            image_url = self.image_url()
-            print('image_url', image_url)
-            self.embed.set_image(url=image_url)
+        if self.image_path or self.thumbnail_path:
+            if self.image_path:
+                self.embed = deepcopy(self.embed_sans_image)  # Puis l'embed avec image
+                self.image_url = Threads(upload_image_bdd(self.image_path, self.image_bdd_table, self.image_keep))
+                image_url = self.image_url()
+                print(f'{image_url = }')
+                self.embed.set_image(url=image_url)
+
+            if self.thumbnail_path:
+                try:  # Copier l'embed qui pourrait déjà contenir une image.
+                    self.embed = deepcopy(self.embed)
+                except Exception as err:  # Sinon, cet embed n'existe pas, donc copier l'embed sans image.
+                    self.embed = deepcopy(self.embed_sans_image)
+                self.thumbnail_url = Threads(upload_image_bdd(self.thumbnail_path, self.thumbnail_bdd_table, self.thumbnail_keep))
+                thumbnail_url = self.thumbnail_url()
+                print(f'{thumbnail_url = }')
+                self.embed.set_thumbnail(url=thumbnail_url)
+
         else:
             self.embed = self.embed_sans_image
 
